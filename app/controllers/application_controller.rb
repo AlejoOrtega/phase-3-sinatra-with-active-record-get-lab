3678ebbbd1deb9b91fd74d6c6abce66d75623159
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-05-28 09:32:45 -0400

    commit tree 6919a78e7a5a1ac150baeee0c32822c6ceed2f69

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
   # add routes
+  get '/' do
+    "Hello World"
+  end
+
+  get '/bakeries' do
+    response = Bakery.all
+    response.to_json
+  end
+
+  get '/bakeries/:id' do
+    response = Bakery.find(params[:id])
+    response.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    response = BakedGood.order('price desc')
+    response.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    response = BakedGood.order('price desc').first
+    response.to_json
+  end
+
+  
 
 end
